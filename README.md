9ac87b3887413730805d8591408a4d58b5f9631f
J Diaz <j.e.diaz@icloud.com>
2021-04-23 17:31:23 -0700

    style adjustments

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ https://j-diaz103.github.io/Bucket-List/
 worked on making the fetch request display in a card.
 with the  fetch we get the img,genres,description, and title
 --pending group review and approval
+
+4/23/21 JD
+Altered the HTML to display the sections like we had discused
--- js/script.js
+++ js/script.js
@@ -17,33 +17,34 @@ fetch(movieSearchUrl)
   .then(function (data) {
     //I used the data to populate part of the results
     //only displays one
-    console.log(data);// json 
-    console.log(data.genres); //gives the Genres
-    console.log(data.original_title); //gives the Movie tittle
-    console.log(data.overview); //gives the movie description
-    console.log(data.poster_path); //gives the path to get the image. not the image itself
+    // console.log(data);// json 
+    // console.log(data.genres); //gives the Genres
+    // console.log(data.original_title); //gives the Movie tittle
+    // console.log(data.overview); //gives the movie description
+    // console.log(data.poster_path); //gives the path to get the image. not the image itself
     //console.log(data.results[i].title); 
     
-    for(i=0; i<10; i++){ $("#results").append(`    <div class="card result-cards">
-    <div class="card-image">
-      <figure class="image is-4by3">
-        <img src="http://image.tmdb.org/t/p/w500/${data.results[i].poster_path}" alt="${data.original_title}">
-      </figure>
-    </div>
-    <div class="card-content">
-      <div class="media">
-        <div class="media-content">
-          <p class="title is-4">${data.results[i].title}</p>
-        </div>
+    for(i=0; i<10; i++){ $("#results").append(`    
+    <div class="card column ">
+      <div class="card-image ">
+        <figure class="image is-4by3">
+          <img src="http://image.tmdb.org/t/p/w500/${data.results[i].poster_path}" alt="${data.results[i].original_title}">
+        </figure>
       </div>
-  
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">${data.results[i].title}</p>
+          </div>
+        </div>
+
       <div class="content">
       <p class="movie-overview"> movie summary</p>
       <p>${data.results[i].overview}</p>
        
+        </div>
       </div>
-    </div>
-  </div>`);
+    </div>`);
     }
   });
 });
@@ -53,51 +54,51 @@ fetch(movieSearchUrl)
   
 
 
-
-//
-// const movieSearchUrl = "https://api.themoviedb.org/3/search" + movieApiKey + "&page=1";
-var movieSearchUrl=`https://api.themoviedb.org/3/search/movie?api_key=${movieApiKey}&query=${searchTerm}`;
-//var movieSearchUrl=`https://api.themoviedb.org/3/movie/${searchedMovie}?api_key=${movieApiKey}&language=en-US`;
-fetch(movieSearchUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    //I used the data to populate part of the results
-    //only displays one
-    console.log(data);// json 
-    console.log(data.genres); //gives the Genres
-    console.log(data.original_title); //gives the Movie tittle
-    console.log(data.overview); //gives the movie description
-    console.log(data.poster_path); //gives the path to get the image. not the image itself
-    $("#results").append(`    <div class="card result-cards">
-    <div class="card-image">
-      <figure class="image is-4by3">
-        <img src="http://image.tmdb.org/t/p/w500/${data.poster_path}" alt="${data.original_title}">
-      </figure>
-    </div>
-    <div class="card-content">
-      <div class="media">
-        <div class="media-content">
-          <p class="title is-4">${data.original_title}</p>
-        </div>
-      </div>
+// no longer need this correct code is in line 1-49
+// //
+// // const movieSearchUrl = "https://api.themoviedb.org/3/search" + movieApiKey + "&page=1";
+// var movieSearchUrl=`https://api.themoviedb.org/3/search/movie?api_key=${movieApiKey}&query=${searchTerm}`;
+// //var movieSearchUrl=`https://api.themoviedb.org/3/movie/${searchedMovie}?api_key=${movieApiKey}&language=en-US`;
+// fetch(movieSearchUrl)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     //I used the data to populate part of the results
+//     //only displays one
+//     console.log(data);// json 
+//     console.log(data.genres); //gives the Genres
+//     console.log(data.original_title); //gives the Movie tittle
+//     console.log(data.overview); //gives the movie description
+//     console.log(data.poster_path); //gives the path to get the image. not the image itself
+//     $("#results").append(`    <div class="card result-cards">
+//     <div class="card-image">
+//       <figure class="image is-4by3">
+//         <img src="http://image.tmdb.org/t/p/w500/${data.poster_path}" alt="${data.original_title}">
+//       </figure>
+//     </div>
+//     <div class="card-content">
+//       <div class="media">
+//         <div class="media-content">
+//           <p class="title is-4">${data.original_title}</p>
+//         </div>
+//       </div>
   
-      <div class="content">
-      <p class="movie-overview"> movie summary</p>
-      ${data.overview}
-        <ul>
-            <p> Genres:</p>
-            <li id="genre1">${data.genres[0].name}</li>
-            <li id="genre2">${data.genres[1].name}</li>
-            <li id="genre3">${data.genres[2].name}</li>
-        </ul>
-      </div>
-    </div>
-  </div>`);
-    }
-  );
-//this is the initial link to add the and image
-//  http://image.tmdb.org/t/p/w500/
+//       <div class="content">
+//       <p class="movie-overview"> movie summary</p>
+//       ${data.overview}
+//         <ul>
+//             <p> Genres:</p>
+//             <li id="genre1">${data.genres[0].name}</li>
+//             <li id="genre2">${data.genres[1].name}</li>
+//             <li id="genre3">${data.genres[2].name}</li>
+//         </ul>
+//       </div>
+//     </div>
+//   </div>`);
+//     }
+//   );
+// //this is the initial link to add the and image
+// //  http://image.tmdb.org/t/p/w500/
 
 

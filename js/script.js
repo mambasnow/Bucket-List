659af90f085d4dd9ef9a1f16e2035d22376278f4
Minh Nguyen <mnguyen728@hotmail.com>
2021-04-24 13:47:04 -0700

    added genre button functionality changed html ids for genre button

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ const movieApiKey = "f59588f382c7895cd8d35268297e4979";
 var resultArea = document.querySelector("#results");
 
 
+
+
 $("#searchBtn").click(function(){ 
  
   var movieSearchUrl=`https://api.themoviedb.org/3/search/movie?api_key=${movieApiKey}&query=${$("#movieSearch").val() }`;
@@ -24,8 +26,12 @@ fetch(movieSearchUrl)
     // console.log(data.poster_path); //gives the path to get the image. not the image itself
     //console.log(data.results[i].title); 
     
+
+    //empties the results of movies
+    $('#results').empty();
+
     for(i=0; i<10; i++){ $("#results").append(`    
-    <div class="card column ">
+    <div id="movieSearches" class="card column ">
       <div class="card-image ">
         <figure class="image is-4by3">
           <img src="http://image.tmdb.org/t/p/w500/${data.results[i].poster_path}" alt="${data.results[i].original_title}">
@@ -49,56 +55,56 @@ fetch(movieSearchUrl)
   });
 });
 
-   
-    
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+
+
+// function that will run inside click for genres   
+ function genreButtonEvent(event){
+
+  let randomPage = getRandomInt(500);
+  let selectedMovieId = $(this).attr("id");
+  let genreSearchURL = `https://api.themoviedb.org/3/genre/${selectedMovieId}/movies?api_key=${movieApiKey}&language=en-US&page=${randomPage}`;
   
 
+  fetch(genreSearchURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data){
+    console.log(data);
 
-// no longer need this correct code is in line 1-49
-// //
-// // const movieSearchUrl = "https://api.themoviedb.org/3/search" + movieApiKey + "&page=1";
-// var movieSearchUrl=`https://api.themoviedb.org/3/search/movie?api_key=${movieApiKey}&query=${searchTerm}`;
-// //var movieSearchUrl=`https://api.themoviedb.org/3/movie/${searchedMovie}?api_key=${movieApiKey}&language=en-US`;
-// fetch(movieSearchUrl)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     //I used the data to populate part of the results
-//     //only displays one
-//     console.log(data);// json 
-//     console.log(data.genres); //gives the Genres
-//     console.log(data.original_title); //gives the Movie tittle
-//     console.log(data.overview); //gives the movie description
-//     console.log(data.poster_path); //gives the path to get the image. not the image itself
-//     $("#results").append(`    <div class="card result-cards">
-//     <div class="card-image">
-//       <figure class="image is-4by3">
-//         <img src="http://image.tmdb.org/t/p/w500/${data.poster_path}" alt="${data.original_title}">
-//       </figure>
-//     </div>
-//     <div class="card-content">
-//       <div class="media">
-//         <div class="media-content">
-//           <p class="title is-4">${data.original_title}</p>
-//         </div>
-//       </div>
+    // need to grab 10 random movies from random pages
+    $('#results').empty();
+
+    for(i=0; i<10; i++){ $("#results").append(`    
+    <div id="movieSearches" class="card column ">
+      <div class="card-image ">
+        <figure class="image is-4by3">
+          <img src="http://image.tmdb.org/t/p/w500/${data.results[i].poster_path}" alt="${data.results[i].original_title}">
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">${data.results[i].title}</p>
+          </div>
+        </div>
+
+      <div class="content">
+      <p class="movie-overview"> movie summary</p>
+      <p>${data.results[i].overview}</p>
+       
+        </div>
+      </div>
+    </div>`);
+    }
+  });
+
+ }   
   
-//       <div class="content">
-//       <p class="movie-overview"> movie summary</p>
-//       ${data.overview}
-//         <ul>
-//             <p> Genres:</p>
-//             <li id="genre1">${data.genres[0].name}</li>
-//             <li id="genre2">${data.genres[1].name}</li>
-//             <li id="genre3">${data.genres[2].name}</li>
-//         </ul>
-//       </div>
-//     </div>
-//   </div>`);
-//     }
-//   );
-// //this is the initial link to add the and image
-// //  http://image.tmdb.org/t/p/w500/
-
-
+/// populates movies accordingly to selected genre
+$('.genreButton').click(genreButtonEvent);
